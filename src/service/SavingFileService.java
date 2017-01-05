e86c95eab1271b4e569854b9d63cf0e92f4de4f3
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-01-05 21:13:22 +0100

    Update save method in SavingFileService

--- src/service/SavingFileService.java
+++ src/service/SavingFileService.java
@@ -4,16 +4,17 @@ import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * Created by Krzysztof on 2017-01-05.
  */
 public class SavingFileService {
 
-    TextArea textArea;
+    private TextArea textArea;
+    private PrintWriter writer;
+    private FileWriter fileWriter;
+    private BufferedWriter buffWriter;
 
     public SavingFileService(TextArea textArea){
         this.textArea = textArea;
@@ -23,6 +24,19 @@ public class SavingFileService {
 
         FileChooser fileChooser = new FileChooser();
         File savedFile = fileChooser.showSaveDialog(stageWindow);
+        
+        try {
+
+            writer = new PrintWriter(savedFile);
+            writer.println(textArea.getText());
+            writer.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
