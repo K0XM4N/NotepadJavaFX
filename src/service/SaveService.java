8f8dba897d135b6f0b8bceeb604bb61018648f9a
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-01-10 15:21:12 +0100

    Removing useless fields from SaveService superclass, writeToFile method added.

--- src/service/SaveService.java
+++ src/service/SaveService.java
@@ -3,22 +3,46 @@ package service;
 import javafx.scene.control.TextArea;
 import model.FileModel;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Created by Krzysztof on 2017-01-10.
  */
 public class SaveService {
 
-    protected TextArea textArea;
-    protected PrintWriter writer;
-    protected FileWriter fileWriter;
-
+    protected TextArea textArea;;
     protected FileModel fileModel;
 
     public SaveService(TextArea textArea, FileModel fileModel){
         this.textArea = textArea;
         this.fileModel = fileModel;
+    }
+
+
+
+    /**
+     * writeToFile Method - Writes text from TextArea (actually from FileModel's contentBeforeSave field) to a file.txt
+     * where file - name of txt file
+     * **/
+    private void writeToFile(String textContent, File savedFile) throws IOException {
+
+        fileModel.setAllFields(savedFile.getAbsolutePath(),savedFile.getName(),textContent,true);
+        FileWriter fileWriter = new FileWriter(savedFile);
+        PrintWriter writer = new PrintWriter(fileWriter);
+        Scanner inputText = new Scanner(textContent);
+
+        while (inputText.hasNextLine()) {
+            writer.print(inputText.nextLine());
+            if (inputText.hasNextLine()) {
+                writer.print("\r\n");
+            }
+        }
+        writer.close();
+        inputText.close();
+
     }
 }
